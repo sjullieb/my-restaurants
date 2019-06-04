048c15732645b53e0fd3bb2c32957c8535d2408f
Student <example@example.com>
2019-06-03 20:54:00 -0700

    add toast on click Find

--- app/src/main/java/com/sjullieb/myrestaurants/MainActivity.kt
+++ app/src/main/java/com/sjullieb/myrestaurants/MainActivity.kt
@@ -2,11 +2,20 @@ package com.sjullieb.myrestaurants
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findRestaurantBtn.setOnClickListener(){
+            println("Button clicked")
+            Toast.makeText(this, "Hello!", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
+
---
+++ app/src/main/java/com/sjullieb/myrestaurants/RestaurantsActivity.kt
@@ -0,0 +1,12 @@
+package com.sjullieb.myrestaurants
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RestaurantsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurants)
+    }
+}
